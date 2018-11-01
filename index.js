b12d185dad02c6c497136886372c839ae18a92d7
Anatoly Nizhnikov <anatoly.nizhnikov@codeit.com.ua>
2018-11-01 13:33:26 +0200

    Replace function-module on class. Inplement methods for create colors array

--- index.js
+++ index.js
@@ -5,20 +5,49 @@ let options = {
     fieldSize: +document.getElementById('fieldSize').value,
 }
 
-const filler = (function () {
-
-
-    /* Инициализатор начала игры. Точка входа в модуль */
-    function start(options) {
-
+class Game {
+    constructor(options) {
+        this.areaWrapper = options.gameArea;
+        this.controlsWrapper = options.controls;
+        this.colorsArray = this.createColorsArray(options.colorsCount);
+        this.fieldSize = options.fieldSize;
     }
 
+    start() {
+
+        this.createControls(this.colorsArray);
 
 
-    return {
-        start
+        this.areaWrapper.appendChild(this.table);
     }
-}());
 
+    createColorsArray(colorsCount) {
+        const colors = [];
+        const randHEX = function () {
+            return (this.randomValue(0, 255)).toString(16);
+        }.bind(this);
 
-filler.start(options);
+        for (let i = 0; i < colorsCount; i++) {
+            colors.push('#' + randHEX() + randHEX() + randHEX());
+        }
+        return colors;
+    }
+
+    /* Генерирует панельку с цветами-кнопками для пользователя */
+    createControls(colors) {
+        let list = document.createElement('ul'),
+            elem = document.createElement('li');
+        list.classList.add('gameControlsList');
+        elem.classList.add('gameControlsElement');
+
+        for (let color of colors) {
+            elem.dataset.color = color;
+            elem.style.backgroundColor = color;
+            list.appendChild(elem.cloneNode(true));
+        }
+        this.controlsWrapper.appendChild(list);
+    }
+}
+
+const game = new Game(options);
+game.start();
